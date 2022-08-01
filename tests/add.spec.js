b45b910425c676138525d5e516469705799091b8
Burak Kantarcı <burak@thundra.io>
2022-08-01 14:22:22 +0300

    changed test url

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -8,7 +8,7 @@ const TODO_ITEMS = [
 ];
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:80/');
+  await page.goto('https://playwright-todo-app.vercel.app/');
 });
 
 test('add a todo item', async ({ page }) => {
