0969b45e492fc515823e662a35cce19675507508
Oguzhan Ozdemir <aoozdemir@live.com>
2022-09-02 16:26:53 +0300

    Fix clear endpoint

--- pages/api/clear.js
+++ pages/api/clear.js
@@ -6,6 +6,6 @@ const redis = new Redis({
 })
 
 export default async (_, res) => {
-    const data = await redis.ltrim('todo_oz', -1, 0)
+    const data = await redis.del('todo_oz')
     return res.status(200).json(data);
 }
